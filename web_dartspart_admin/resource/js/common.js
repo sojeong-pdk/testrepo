ec04103f421bafb9a1cccaa0845299fc61bf5eaf
sojeong-pdk <116628932+sojeong-pdk@users.noreply.github.com>
2022-12-07 17:33:08 +0900

    Add files via upload

--- web_dartspart_admin/resource/js/common.js
+++ web_dartspart_admin/resource/js/common.js
@@ -1,6 +1,12 @@
 const init = () => {
+    // common function list
+    const elBody = document.body;
+    const addCls = (elItem,elClass) => { elItem.classList.add(elClass); }
+    const removeCls = (elItem,elClass) => { elItem.classList.remove(elClass); }
+    const toggleCls = (elItem,elClass) => { elItem.classList.toggle(elClass); }
+
     // header gnb area fetch load event
-  const headerNav = document.querySelector('#header'),
+    const headerNav = document.querySelector('#header'),
         headerNavUrl = headerNav.dataset.include;
 
     fetch(headerNavUrl)
@@ -12,7 +18,7 @@ const init = () => {
         headerNav.removeAttribute('data-include');
 
         // navigation menu tab on style handler
-      const elHeader = document.querySelector("#header"),
+        const elHeader = document.querySelector("#header"),
             elMenuList = document.querySelectorAll(".gnb__menu li");
 
         elMenuList.forEach((item)=>{
@@ -21,14 +27,14 @@ const init = () => {
         })
 
         // modal logout toggle event
-      const btnLogout = document.querySelector(".btn_logout"),
+        const btnLogout = document.querySelector(".btn_logout"),
             btnLogoutCancle = document.querySelector(".modal.logout .btn_cancle"),
             modalLogout = document.querySelector(".modal.logout");
         
         const modalLogoutHandler = (elBtn) => {
             elBtn.addEventListener("click",() => {
-                modalLogout.classList.toggle("on");
-                document.body.classList.toggle("on");
+                toggleCls(modalLogout,"on");
+                toggleCls(elBody,"on");
             })
         }
 
@@ -37,7 +43,7 @@ const init = () => {
     };
 
     // aside top button fetch load event
-  const bntTop = document.querySelector('.btn_top'),
+    const bntTop = document.querySelector('.btn_top'),
         btnTopUrl = bntTop.dataset.include;
 
     fetch(btnTopUrl)
@@ -52,8 +58,8 @@ const init = () => {
         window.addEventListener("scroll",()=>{
             let scrHeight = window.pageYOffset || document.documentElement.scrollTop;
             scrHeight > 100
-            ? btnTop.classList.add("on")
-            : btnTop.classList.remove("on");
+            ? addCls(btnTop,"on")
+            : removeCls(btnTop,"on");
         })
         btnTop.addEventListener("click",()=>{
             window.scrollTo({ top: "0", behavior: "smooth" });
@@ -61,50 +67,52 @@ const init = () => {
     };
 
     // table box checked button click event
-    if( document.body.contains(document.querySelector(".chk_all")) ){
-      const chkAll = document.querySelector(".chk_all"),
+    if( elBody.contains(document.querySelector(".chk_all")) ){
+        const chkAll = document.querySelector(".chk_all"),
             chkOne = document.querySelectorAll(".chk_one"),
             btnCplt = document.querySelector(".btn_cplt"),
             labOne = document.querySelectorAll(".label_one");
+        const checkBoolean = (Boolean)=>{ chkAll.checked = Boolean; };
+        const disabledBoolean = (Boolean)=>{ btnCplt.disabled = Boolean; }
 
-        chkAll.addEventListener("click",() => {
+        chkAll.addEventListener("click",()=>{
             chkAll.checked == true
-            ? chkOne.forEach((item,key) => {
+            ? chkOne.forEach((item,key)=>{
                 item.checked = true;
-                labOne[key].classList.add("checked");
-                btnCplt.disabled = false;
+                disabledBoolean(false);
+                addCls(labOne[key],"checked");
             })
-            : chkOne.forEach((item,key) => {
+            : chkOne.forEach((item,key)=>{
                 item.checked = false;
-                labOne[key].classList.remove("checked");
-                btnCplt.disabled = true;
+                disabledBoolean(true);
+                removeCls(labOne[key],"checked");
             })
         })
-        chkOne.forEach((item,key) => {
-            item.addEventListener("click",() => {
+        chkOne.forEach((item,key)=>{
+            item.addEventListener("click",()=>{
+                const chkOneAll = document.querySelectorAll('input[name="chkOne"]:checked');
+
                 item.checked == true
-                ? labOne[key].classList.add("checked")
-                : labOne[key].classList.remove("checked");
+                ? addCls(labOne[key],"checked")
+                : removeCls(labOne[key],"checked");
 
-                setTimeout(() => {
-                    const chkOneAll = document.querySelectorAll('input[name="chkOne"]:checked');
-
+                setTimeout(()=>{
                     chkOneAll.length < chkOne.length
-                    && (chkAll.checked = false);
+                    && checkBoolean(false);
                     
                     chkOneAll.length == chkOne.length
-                    && (chkAll.checked = true);
+                    && checkBoolean(true);
 
                     chkOneAll.length >= 1
-                    ? btnCplt.disabled = false
-                    : btnCplt.disabled = true;
+                    ? disabledBoolean(false)
+                    : disabledBoolean(true);
                 }, 120);
             })
         })
     }
     // custom select box UI controls select tag
-    if( document.body.contains(document.querySelector(".custom_select")) ){
-      const elSelect = document.querySelector("select"),
+    if( elBody.contains(document.querySelector(".custom_select")) ){
+        const elSelect = document.querySelector("select"),
             selectedArea = document.querySelector(".category"),
             selectBox = document.querySelector(".custom_select"),
             selectUl = document.querySelector(".custom_select ul"),
@@ -113,7 +121,7 @@ const init = () => {
         elSelect.addEventListener("change",() => { selectedArea.innerHTML = this.value; })
 
         selectedArea.addEventListener("click",() => {
-            selectBox.classList.toggle("on");
+            toggleCls(selectBox,"on");
             setTimeout(()=>{ selectUl.scrollTo(0,0); },200);
         })
 
@@ -121,13 +129,13 @@ const init = () => {
             item.addEventListener("click",() => {
                 selectedArea.innerHTML = item.dataset.value;
                 elSelect.value = item.dataset.value;
-                selectBox.classList.remove("on");
+                removeCls(selectBox,"on");
 
                 setTimeout(()=>{ selectUl.scrollTo(0,0); },200);
 
                 key !== 0
-                ? selectedArea.classList.add("off")
-                : selectedArea.classList.remove("off");
+                ? addCls(selectedArea,"off")
+                : removeCls(selectedArea,"off");
             })
         })
     }
@@ -145,13 +153,12 @@ const init = () => {
         
         appVersion.includes("Mobile")
         ? deviceCheck = "Mobile"
-        : deviceCheck = "PC";
+        : deviceCheck = "PC", osCheck = "";
 
-        appVersion.includes("Mac")
-        ? osCheck = "Apple"
-        : osCheck = "Not Apple";
+        appVersion.includes("Mac") && (osCheck = "iOS");
+        appVersion.includes("Android") && (osCheck = "Android");
 
-        console.log( deviceCheck, osCheck, scrollNum );
+        console.log(deviceCheck, osCheck, scrollNum);
     })
 }
 window.addEventListener("load",init);
